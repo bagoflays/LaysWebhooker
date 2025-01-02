97642b8495b8d7c3afd6986ae1a1cca307c8f36b
Lays <108637656+bagoflays@users.noreply.github.com>
2025-01-01 16:25:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 console.log(navigator.userAgent.toLowerCase())
-if (/android|iphone|ipad/i.test(navigator.userAgent.toLowerCase())) {
+if (document.getElementById("stupidtitle")){
+   if (/android|iphone|ipad/i.test(navigator.userAgent.toLowerCase())) {
    document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Mobile"
-} else if (/windows/i.test(navigator.userAgent.toLowerCase())) {
-    document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Windows"
-} else if (/macintosh|mac os/i.test(navigator.userAgent.toLowerCase())) {
-    document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for MacOS"
-} else if (/linux/i.test(navigator.userAgent.toLowerCase())) {
-   document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Linux/Mobile Desktop Mode"
+   } else if (/windows/i.test(navigator.userAgent.toLowerCase())) {
+       document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Windows"
+   } else if (/macintosh|mac os/i.test(navigator.userAgent.toLowerCase())) {
+       document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for MacOS"
+   } else if (/linux/i.test(navigator.userAgent.toLowerCase())) {
+      document.getElementById("stupidtitle").innerText = "Lays Webhooker for Discord for Linux/Mobile Desktop Mode"
+   }
 }
 function jsSend() {
   const url = prompt('Add a Webhook URL (By Lays Webhooker)')
